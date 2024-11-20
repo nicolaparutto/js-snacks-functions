a83305495bd9fe867ce19ca7c1a5bc6a3a5db56f
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-20 18:33:17 +0100

    feat: end second exercise

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function  paroleLetterA (array){
+   let arrayParole = [];
 
+   for(let i = 0; i < array.length; i++){
+      if(array[i].includes('A')){
+         arrayParole.push(array[i])
+      }
+   }
+   return arrayParole;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(paroleLetterA(names))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
