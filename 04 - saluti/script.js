015962bea54c88ed462c5e4989e87e3a3d9018d1
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-20 18:52:58 +0100

    feat: end fourth exercise

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,13 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(nomeInserito){
+   let saluto = `Ciao ${nomeInserito}`;
+   return saluto;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
