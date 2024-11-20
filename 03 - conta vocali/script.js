50d4ceab51b88db3b714bcc8842f8f7be8a21b69
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-20 19:09:35 +0100

    feat: end third exercise

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,26 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function trovaVocali(parola){
+   let vocali = 'aeiou';
+   let totaleVocaliPerParola = 0;
+
+   for(let i = 0; i < parola.length; i++){
+      if(vocali.indexOf(parola[i]) !== -1){
+         totaleVocaliPerParola += 1;
+      }
+   }
+   return totaleVocaliPerParola;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(trovaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
+
+
+//bisogna prendere ogni carattere della stringa e confrontarlo con ogni vocale, se sono uguali salvarle in una stringa 
